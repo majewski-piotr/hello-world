030190c34c37988aec9613f9d66f76ce11ac968f
Piotr Majewski <majewski.piotr.511@gmail.com>
2026-04-26 06:52:31 +0200

    feat: terraform

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      project = "my-gallery"
+      created = "terraform"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+
+data "aws_region" "current" {}
---
+++ terraform/vars.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type    = string
+  default = "my-gallery"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
